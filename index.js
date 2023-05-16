76f724d409846bf35623ea090aa6c769f90c4451
quangphat18ti <70308391+quangphat18ti@users.noreply.github.com>
2023-05-16 16:29:12 +0700

    b

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use("/api/websiteAPI", routers.websiteAPI);
 
 // task.start();
 
-updateDB();
+// updateDB();
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
